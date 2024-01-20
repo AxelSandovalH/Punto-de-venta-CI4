88eb896663d3a5a87f2f18811401b680639e8f99
Axel <axesan917@gmail.com>
2024-01-19 18:46:10 -0600

    Commit inicial

---
+++ app/Views/footer.php
@@ -0,0 +1,21 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; Axel Sandoval <?php echo date('Y'); ?></div>
+            <div>
+                <a href="https://github.com/AxelSandovalH" target="_blank">Github</a>
+                &middot;
+                <a href="https://axelsandovalh.github.io/" target="_blank">Host</a>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/simple-datatables.min.js"></script>
+<script src="js/datatables-simple-demo.js"></script>
+</body>
+
+</html>
